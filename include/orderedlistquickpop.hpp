b381f3dff1b95416daf0f1edac4a44238031ed5d
MoJo-JoJo24 <132806997+MoJo-JoJo24@users.noreply.github.com>
2023-05-07 11:25:23 +0300

    Add files via upload

---
+++ include/orderedlistquickpop.hpp
@@ -0,0 +1,54 @@
+#ifndef __QUICKPOPORDEREDLIST_HPP__
+#define __QUICKPOPORDEREDLIST_HPP__
+
+#include "orderedList.hpp"
+
+template <class T>
+class QuickPopOrderedList : public OrderedList<T>
+{
+    public:
+        QuickPopOrderedList(std::function<int(const T*, const T*)> cmp_func_);
+        ~QuickPopOrderedList();
+        virtual T &Pop(); // O(1) time complexity
+        virtual void Push(T &data_); // O(n) time complexity
+};
+
+////////// Implementations ////////////////////////////////////////////////////
+
+
+template <class T>
+T& QuickPopOrderedList<T>::Pop()
+{
+    sem_wait(this->GetSemaphore());
+
+    std::unique_lock<std::shared_mutex> shared_lock(*this->GetSharedSearchMutex());
+    std::unique_lock<std::shared_mutex> shared_lock2(*this->GetSharedInsertMutex());
+
+    ListNode<T>* holding_ptr = this->Begin();
+    
+    T &data = holding_ptr->val;
+    this->SetBegin(holding_ptr->next);
+    
+    delete holding_ptr;
+
+    return data;
+}
+
+template <class T>
+void QuickPopOrderedList<T>::Push(T &data_)
+{
+    return OrderedList<T>::Push(data_);
+}
+
+
+template <class T>
+QuickPopOrderedList<T>::QuickPopOrderedList(std::function<int(const T*, const T*)> cmp_func_)
+: OrderedList<T>(cmp_func_)
+{}
+
+template <class T>
+QuickPopOrderedList<T>::~QuickPopOrderedList()
+{}
+
+
+#endif /* __QUICKPOPORDEREDLIST_HPP__ */
---
+++ include/orderedlistquickpush.hpp
@@ -0,0 +1,49 @@
+#ifndef __QUICKPUSHORDEREDLIST_HPP__
+#define __QUICKPUSHORDEREDLIST_HPP__
+
+#include "orderedList.hpp"
+
+template <class T>
+class QuickPushOrderedList : public OrderedList<T>
+{
+    public:
+        QuickPushOrderedList(std::function<int(const T*, const T*)> cmp_func_);
+        ~QuickPushOrderedList();
+        virtual T &Pop(); // O(n) time complexity
+        virtual void Push(T &data_); // O(1) time complexity
+};
+
+////////// Implementations ////////////////////////////////////////////////////
+
+
+template <class T>
+T& QuickPushOrderedList<T>::Pop()
+{
+    return  OrderedList<T>::Pop();
+}
+
+template <class T>
+void QuickPushOrderedList<T>::Push(T &data_)
+{
+    ListNode<T>* new_node = new ListNode<T>(data_);
+
+    std::shared_lock<std::shared_mutex> shared_lock(*this->GetSharedInsertMutex()); 
+    std::unique_lock<std::mutex> lock(*this->GetMutex());
+
+    new_node->next = this->Begin();
+    this->SetBegin(new_node);
+    
+    sem_post(this->GetSemaphore());          
+}
+
+template <class T>
+QuickPushOrderedList<T>::QuickPushOrderedList(std::function<int(const T*, const T*)> cmp_func_)
+: OrderedList<T>(cmp_func_)
+{}
+
+template <class T>
+QuickPushOrderedList<T>::~QuickPushOrderedList()
+{}
+
+
+#endif /* __QUICKPUSHORDEREDLIST_HPP__ */
